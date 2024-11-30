1e3f11c61381b707d1edf45ccaf39764e4a3422b
Yaro Shm <yshmarov@gmail.com>
2024-11-30 22:13:48 +0100

    Update path_configuration_controller.rb
    https://github.com/hotwired/hotwire-native-ios/blob/main/Demo/path-configuration.json

--- lib/generators/hotwire_native/templates/controllers/hotwire_native/v1/ios/path_configuration_controller.rb
+++ lib/generators/hotwire_native/templates/controllers/hotwire_native/v1/ios/path_configuration_controller.rb
@@ -1,5 +1,49 @@
 class HotwireNative::V1::Ios::PathConfigurationsController < ActionController::Base
   def show
-    render json: {}
+    render json:
+      {
+        "settings": {
+          "enable_feature_x": true
+        },
+        "rules": [
+          {
+            "patterns": [
+              "/new$",
+              "/edit$",
+              "/signin$",
+              "/strada-form$"
+            ],
+            "properties": {
+              "context": "modal",
+              "pull_to_refresh_enabled": false
+            }
+          },
+          {
+            "patterns": [
+              "/numbers$"
+            ],
+            "properties": {
+              "view_controller": "numbers"
+            }
+          },
+          {
+            "patterns": [
+              "/numbers/[0-9]+$"
+            ],
+            "properties": {
+              "view_controller": "numbers_detail",
+              "context": "modal"
+            }
+          },
+          {
+            "patterns": [
+              "^/$"
+            ],
+            "properties": {
+              "presentation": "replace_root"
+            }
+          },
+        ]
+      }
   end
 end
